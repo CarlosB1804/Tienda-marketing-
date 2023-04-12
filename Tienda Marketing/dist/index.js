171ecd36fa47b8f72f65cd2a3fcd3841f1bf1eb2
sebreyes9999 <118933459+sebreyes9999@users.noreply.github.com>
2023-04-11 23:42:02 -0500

    Update index.js

--- Tienda Marketing/dist/index.js
+++ Tienda Marketing/dist/index.js
@@ -1,35 +1,32 @@
-var swiper = new Swiper("mySwiper-1",   {
+var swiper = new Swiper(".mySwiper-1" ,{
     slidesPerView:1,
     spaceBetween: 30,
     loop:true,
     pagination: {
         el:".swiper-pagination",
-        clickable:true, 
+        clickable:true,
     },
-    navigation: {
+    navigation:{
         nextEl:".swiper-button-next",
-        pervEl:".swiper-button-prev"
+        prevEl:".swiper-button-prev"
     }
 });
 
-var swiper = new Swiper("mySwiper-2",   {
-    slidesPerView:4,
+var swiper = new Swiper(".mySwiper-2" ,{
+    slidesPerView:2,
     spaceBetween: 30,
     loop:true,
-    loopFillGroupWithBlank: true,
-    navigation: {
+    loopFillGroupWithblank:true,
+    navigation:{
         nextEl:".swiper-button-next",
-        pervEl:".swiper-button-prev"
+        prevEl:".swiper-button-prev"
     },
     breakpoints : {
         0:{
             slidesPerView:1
         },
-        520: {
+        520:{
             slidesPerView:2
         },
-        950:{
-            slidesPerView:3
-        }
     }
 });
