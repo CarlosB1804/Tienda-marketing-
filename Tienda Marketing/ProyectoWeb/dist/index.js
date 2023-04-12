c6c6df4a881a89363e4a3f697a779afdbe4ccc73
sebreyes9999 <118933459+sebreyes9999@users.noreply.github.com>
2023-04-12 02:38:59 -0500

    Add files via upload

---
+++ Tienda Marketing/ProyectoWeb/dist/index.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".mySwiper-1" ,{
+    slidesPerView:1,
+    spaceBetween: 30,
+    loop:true,
+    pagination: {
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    }
+});
+
+var swiper = new Swiper(".mySwiper-2" ,{
+    slidesPerView:2,
+    spaceBetween: 30,
+    loop:true,
+    loopFillGroupWithblank:true,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    breakpoints : {
+        0:{
+            slidesPerView:1
+        },
+        520:{
+            slidesPerView:2
+        },
+    }
+});
